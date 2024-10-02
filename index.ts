23271d1e00bd9efd4b3b3466f13df72ac48c5b13
Masui <masanori840816@gmail.com>
2024-10-03 03:55:13 +0900

    CORS

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import express from "express";
+import cors from "cors";
 import { loadSettings } from "./appSettings";
 import path from "path";
 
 const port = 3000;
 const app = express();
 const rootDir = __dirname.replace(/\\/g, "/").replace("/js", "");
+const allowlist = ['http://localhost:3000', 'http://localhost:5173']
+const corsOptionsDelegate: cors.CorsOptionsDelegate<any> = (req, callback) => {
+  const corsOptions = (allowlist.indexOf(req.header('Origin')) !== -1)? { origin: true }: { origin: false };
+  callback(null, corsOptions);
+};
 
 loadSettings(rootDir)
 .then(settings => {
     app.use(express.json());
+    app.use(cors(corsOptionsDelegate));
     app.use(express.static("clients/dist"));
     app.use(express.static(settings.pointCloudDir))
     app.get("/pointclouds", (_, res) => {
